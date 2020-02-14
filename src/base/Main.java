5798075a0d1f1bf4bd60f8ebe5ba481274618a4c
amitofile <amitofile@gmail.com>
2020-02-15 02:41:13 +0530

    type listing done

--- src/base/Main.java
+++ src/base/Main.java
@@ -7,16 +7,31 @@ package base;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
-    static int i = 0;
-    
+    static List<String> resultPDF = new ArrayList<>();
+    static List<String> resultPPT = new ArrayList<>();
+    static List<String> resultDOC = new ArrayList<>();
+    static List<String> resultTXT = new ArrayList<>();
 
     public static void main(String[] args) {
         File currentDir = new File("C:\\Users\\amit\\Downloads"); // current directory
         scanDirRecursive(currentDir);
-        System.out.println(i);
+
+        resultPDF.forEach((string) -> {
+            System.out.println(string);
+        });
+        System.out.println("");
+        resultPPT.forEach(System.out::println);
+        System.out.println("");
+        resultDOC.forEach(System.out::println);
+        System.out.println("");
+        resultTXT.forEach(System.out::println);
+
     }
 
     public static void scanDirRecursive(File dir) {
@@ -24,13 +39,27 @@ public class Main {
             File[] files = dir.listFiles();
             for (File file : files) {
                 if (file.isDirectory()) {
-                    System.out.println("directory:" + file.getCanonicalPath());
+                    //System.out.println("directory:" + file.getCanonicalPath());
                     scanDirRecursive(file);
                 } else {
                     String file_name = file.getName();
-                    
-                    System.out.println("file:" + file.getCanonicalPath());
-                    i++;
+
+                    switch (file_name.substring(file_name.lastIndexOf('.') + 1)) {
+                        case "pdf":
+                            resultPDF.add(file.getCanonicalPath());
+                            break;
+                        case "ppt":
+                        case "pptx":
+                            resultPPT.add(file.getCanonicalPath());
+                            break;
+                        case "doc":
+                        case "docx":
+                            resultDOC.add(file.getCanonicalPath());
+                            break;
+                        case "txt":
+                            resultTXT.add(file.getCanonicalPath());
+                            break;
+                    }
                 }
             }
         } catch (IOException e) {
