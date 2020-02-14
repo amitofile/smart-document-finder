ad0aed496281889c4346853807b6209f3b91863a
amitofile <amitofile@gmail.com>
2020-02-15 02:21:45 +0530

    Code update - 20200215

--- src/base/Main.java
+++ src/base/Main.java
@@ -7,59 +7,33 @@ package base;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.DirectoryStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.function.Function;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
-/**
- *
- * @author amit
- */
 public class Main {
 
-//    public static void main(String[] args) throws IOException {
-//        Path start = Paths.get("C:\\\\Users\\\\amit\\\\Downloads\\");
-//        try (Stream<Path> stream = Files.walk(start, Integer.MAX_VALUE)) {
-//            List<String> collect = stream
-//                    .map(String::valueOf)
-//                    .sorted()
-//                    .collect(Collectors.toList());
-//            int i = 0;
-//            i = collect.stream().map((String string) -> {
-//                System.out.println(string);
-//                return string;
-//            }).map((_item) -> 1).reduce(i, Integer::sum);
-//            System.out.println(i);
-//        }
-//    }
+    static int i = 0;
+    
 
-//    public static void main(String[] args) {
-//
-//        List<Path> resultPDF = new ArrayList<>();
-//        List<Path> resultPPT = new ArrayList<>();
-//        List<Path> resultDOC = new ArrayList<>();
-//        List<Path> resultTXT = new ArrayList<>();
-//
-//        File[] directories = new File("C:\\Users\\amit\\Downloads").listFiles(File::isDirectory);
-//
-//        for (File directory : directories) {
-//            Main.getAllFiles(directory.getPath());
-//        }
-//    }
-//
-//    private static void getAllFiles(String dir) {
-//        Path folder = Paths.get(dir);
-//        try (DirectoryStream<Path> stream = Files.newDirectoryStream(folder)) {
-//            for (Path entry : stream) {
-//                System.out.println(entry);
-//            }
-//        } catch (IOException ex) {
-//        }
-//    }
+    public static void main(String[] args) {
+        File currentDir = new File("C:\\Users\\amit\\Downloads"); // current directory
+        scanDirRecursive(currentDir);
+        System.out.println(i);
+    }
+
+    public static void scanDirRecursive(File dir) {
+        try {
+            File[] files = dir.listFiles();
+            for (File file : files) {
+                if (file.isDirectory()) {
+                    System.out.println("directory:" + file.getCanonicalPath());
+                    scanDirRecursive(file);
+                } else {
+                    String file_name = file.getName();
+                    
+                    System.out.println("file:" + file.getCanonicalPath());
+                    i++;
+                }
+            }
+        } catch (IOException e) {
+        }
+    }
 }
