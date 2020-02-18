f48dd69d27b94c3ab45c04a45b23616c1440252c
amit.patil <amit.patil@tejora.com>
2020-02-18 22:31:53 +0530

    Name detection test completed

--- src/base/TestNLP.java
+++ src/base/TestNLP.java
@@ -7,9 +7,12 @@ package base;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import opennlp.tools.namefind.NameFinderME;
+import opennlp.tools.namefind.TokenNameFinderModel;
 import opennlp.tools.sentdetect.SentenceDetectorME;
 import opennlp.tools.sentdetect.SentenceModel;
 import opennlp.tools.tokenize.SimpleTokenizer;
+import opennlp.tools.util.Span;
 
 /**
  *
@@ -17,17 +20,17 @@ import opennlp.tools.tokenize.SimpleTokenizer;
  */
 public class TestNLP {
 
-    final static String SENTENCE = "I am thrilled to welcome the Class of 2017 to our Graduation Party. I can scarcely believe we are graduates – it really is a time for a huge celebration. I love the party venue already, it is outstanding!";
+    final static String SENTENCE = "Ronan: This means 'little seal. .... Brayden: It means 'a broad hillside. .... Hugo: The name means 'intelligent. .... Diego: The name means 'the famous bearer' in Spanish. .... Antonio: It signifies someone who is 'beyond praise. .... Marco: The name means 'warring. .... Steffan: .... Devin:";
 
     public static void main(String[] args) {
 
         try {
             //Loading sentence detector model 
             InputStream inputStream = new FileInputStream("./OpenNLP_models/en-sent.bin");
-            SentenceModel model = new SentenceModel(inputStream);
+            SentenceModel sent_model = new SentenceModel(inputStream);
 
             //Instantiating the SentenceDetectorME class 
-            SentenceDetectorME detector = new SentenceDetectorME(model);
+            SentenceDetectorME detector = new SentenceDetectorME(sent_model);
 
             //Detecting the sentence
             String sentences[] = detector.sentDetect(SENTENCE);
@@ -35,13 +38,27 @@ public class TestNLP {
             //Instantiating SimpleTokenizer class 
             SimpleTokenizer simpleTokenizer = SimpleTokenizer.INSTANCE;
 
+            //Loading the NER-person model 
+            InputStream inputStreamNameFinder = new FileInputStream("./OpenNLP_models/en-ner-person.bin");
+            TokenNameFinderModel person_model = new TokenNameFinderModel(inputStreamNameFinder);
+
+            //Instantiating the NameFinderME class 
+            NameFinderME nameFinder = new NameFinderME(person_model);
+
             //Printing the sentences 
-            for (String sent : sentences) {
-                System.out.println(sent);
-                String tokens[] = simpleTokenizer.tokenize(sent); 
-                for(String token : tokens){
-                    System.out.println(token);
+            for (String sentence : sentences) {
+                System.out.println(sentence);
+
+                String tokens[] = simpleTokenizer.tokenize(sentence);
+                for (String token : tokens) {
+                    //System.out.println(token);
                 }
+
+                Span names[] = nameFinder.find(tokens);
+                for (Span name : names) {
+                    System.out.println(tokens[name.getStart()]);
+                }
+
                 System.err.println("---------------------");
             }
 
