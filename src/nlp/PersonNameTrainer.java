11229922399ba88220d6f4264e10e86b11f356d0
amit.patil <amit.patil@tejora.com>
2020-02-19 22:46:40 +0530

    Trainer added

---
+++ src/nlp/PersonNameTrainer.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nlp;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
+import opennlp.tools.namefind.NameFinderME;
+import opennlp.tools.namefind.NameSample;
+import opennlp.tools.namefind.NameSampleDataStream;
+import opennlp.tools.namefind.TokenNameFinderFactory;
+import opennlp.tools.namefind.TokenNameFinderModel;
+import opennlp.tools.util.MarkableFileInputStreamFactory;
+import opennlp.tools.util.ObjectStream;
+import opennlp.tools.util.PlainTextByLineStream;
+import opennlp.tools.util.TrainingParameters;
+
+/**
+ *
+ * @author tejora
+ */
+public class PersonNameTrainer {
+
+    public static void main(String[] args) {
+        try {
+            ObjectStream<String> lineStream = new PlainTextByLineStream(new MarkableFileInputStreamFactory(new File("en-ner-person.train")), StandardCharsets.UTF_8);
+
+            TokenNameFinderModel model;
+
+            try (ObjectStream<NameSample> sampleStream = new NameSampleDataStream(lineStream)) {
+                model = NameFinderME.train("en", "person", sampleStream, TrainingParameters.defaultParams(), null);
+                sampleStream.close();
+            }
+
+            try (OutputStream modelOut = new BufferedOutputStream(new FileOutputStream(new File("en-ner-person.bin")))) {
+                model.serialize(modelOut);
+            }
+            System.out.println("\tmodel generated");
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
+    }
+}
